d90fa9de470bf066ccfa1a48d44dd1ac483a583b
Ryan McGowan <ryan@ryanmcg.com>
2014-02-15 13:41:42 -0800

    Inc version to 0.1.0.

--- project.clj
+++ project.clj
@@ -1,9 +1,9 @@
-(defproject incise-base-hiccup-layouts "0.1.0-SNAPSHOT"
+(defproject incise-base-hiccup-layouts "0.1.0"
   :description "Some very basic, hiccup layouts for incise."
   :url "https://github.com/RyanMcG/incise-base-hiccup-layouts"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[incise-core "0.2.0-SNAPSHOT"]
+  :dependencies [[incise-core "0.2.0"]
                  [hiccup "1.0.5"]]
   :repl-options {:init-ns incise.repl}
   :main incise.core)
